955122465f0c06723fd1ebc28409679eda739b9c
t4tomer <tomer.polsky27@gmail.com>
2022-04-07 21:07:08 +0300

    update

--- recursion_101.java
+++ recursion_101.java
@@ -7,8 +7,11 @@ public class recursion_101
       //This is the print line. Anything in the Quotation Maks will be printed
         System.out.println("Hello, World!");
         starts(3);
+        int[] a = {1, 2, 3,4};
         // System.out.println("sum:"+sum(321) );
-        System.out.println(" the multiplyction :"+multiply(3,2) );
+        // System.out.println("the multiplyction :"+multiply(3,2) );
+        // System.out.println("the sum of array is :"+sum_arr(a) );
+        System.out.println("the max val:"+max_arr(a,0));
     }
 
     public static void starts(int n)
@@ -43,6 +46,41 @@ public class recursion_101
         return fibo(n-1)+fibo(n-2);
     }
 
-    
+    /* sum of array using recursion */
+    public static int sum_arr(int [] a)
+    {
+        return sum_arr(a,0);
+    }
+    /* over loading of method sum_arr */
+    public static int sum_arr(int [] a,int start)
+    {
+        if(start==a.length)
+            return 0;
+        return sum_arr(a,start+1)+a[start];
+    }    
+
+    public static int max_arr(int [] a)
+    {
+        return max_arr(a,0);
+    }
+    /* over loading of method sum_arr */
+    public static int max_arr(int [] a,int start)
+    {
+        if(start==a.length-1)
+            return a[a.length-1];
+        return Math.max(a[start],a[start+1]);
+
+    }    
+
+
+
+
+
+
+
+
+
+
+
 
 }
